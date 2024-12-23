726c7f02afb350f45a572b7a4dec72d1087f86fc
mskz-3110 <mskz.saito@gmail.com>
2024-12-24 00:43:50 +0900

    Build macos

--- c/cbuildon.py
+++ c/cbuildon.py
@@ -3,6 +3,40 @@ import os
 sys.path.append("""{}/build""".format(os.path.dirname(__file__)))
 from cbuildon_scripts import *
 
+def cmake_build_macos(isClean):
+  configurations = ["Debug", "Release"]
+  for configuration in configurations:
+    buildDirectory = """build/{}""".format(configuration)
+    if os.path.isdir(buildDirectory) is False:
+      mkdir(buildDirectory)
+      command([
+        "cmake",
+        "-G", "Xcode",
+        "-D", "CMAKE_OSX_ARCHITECTURES='x86_64;arm64'",
+        "-D", """CMAKE_BUILD_TYPE={}""".format(configuration),
+        "-B", buildDirectory,
+      ])
+    elif isClean:
+      command([
+        "cmake",
+        "--build", buildDirectory,
+        "--target", "clean",
+      ])
+    command([
+      "cmake",
+      "--build", buildDirectory
+    ])
+
+def macos_build(isClean):
+  oldDir = getdir()
+  chdir("build/macos")
+  cmake_build_macos(isClean)
+  chdir(oldDir)
+
+def macos_test(argv):
+  pass
+
+chdir(os.path.dirname(__file__))
 argv = sys.argv[1:]
 taskName = shift(argv, "")
 match taskName:
@@ -26,6 +60,12 @@ match taskName:
     linux_build(True)
   case "linux.test":
     linux_test(argv)
+  case "macos.build":
+    macos_build(False)
+  case "macos.rebuild":
+    macos_build(False)
+  case "macos.test":
+    macos_test(argv)
   case _:
     strings = []
     if 0 < len(taskName):
@@ -50,5 +90,11 @@ match taskName:
   linux.rebuild
 
   linux.test <test names>
+
+  macos.build
+
+  macos.rebuild
+
+  macos.test <test names>
 """)
     sys.exit("\n".join(strings))
