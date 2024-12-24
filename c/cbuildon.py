80a478a0b69a3d960b471b48c19570ec7a34a711
mskz-3110 <mskz.saito@gmail.com>
2024-12-24 20:47:39 +0900

    Build macos

--- c/cbuildon.py
+++ c/cbuildon.py
@@ -10,7 +10,7 @@ def cmake_build_macos(libRootDir, isClean):
   for arch in ["arm64", "x86_64"]:
     for configuration in ["Debug", "Release"]:
       buildDirectory = """build/{}_{}""".format(arch, configuration)
-      libDir = os.path.abspath("""{}/{}""".format(libRootDir, configuration))
+      libDir = os.path.abspath("""{}_{}_{}""".format(libRootDir, arch, configuration))
       buildArgs = [
           "cmake",
           "--build", buildDirectory,
